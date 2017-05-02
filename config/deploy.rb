cd3bb6e7d55ada78a0b511405c45eaaa9807353e
ikuohayakawa <discord.890407@gmail.com>
2017-05-02 17:05:59 +0900

    initial commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "4s"
-set :repo_url, "git@github.com:Ikuo89/schedule-management.git"
+set :repo_url, "git@github.com:Ikuo89/4s.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

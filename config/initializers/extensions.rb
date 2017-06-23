86a3ea989d38d85b0145c12ddc6681b14f966f5e
ikuohayakawa <discord.890407@gmail.com>
2017-06-23 18:50:42 +0900

    move my_warning

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -1,3 +1,4 @@
+Dir["#{Rails.root}/lib/exceptions/concerns/*.rb"].each {|file| require file }
 Dir["#{Rails.root}/lib/exceptions/*.rb"].each {|file| require file }
 Dir["#{Rails.root}/lib/*.rb"].each {|file| require file }
 Dir["#{Rails.root}/app/services/concerns/*.rb"].each {|file| require file }
